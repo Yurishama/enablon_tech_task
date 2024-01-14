fcbddd2fc9103bd79d554096e6a9dc4baedec5b0
Saida Reyes <yurishama@gmail.com>
2024-01-14 13:11:28 +0100

    add two negative test more

--- resources/data.js
+++ resources/data.js
@@ -1,6 +1,8 @@
 let data = {
     task1: 'Walk with doggy',
     task2: "Check emails",
+    task_special: "°!#$%&/(()=?¡*¨])",
+    task_code: "console.log(null)"
 
 }
 
--- specs/negative_test_cases.js
+++ specs/negative_test_cases.js
@@ -6,10 +6,6 @@ fixture('Negativetest to do page')
 
 test('Test case 1: create new tast 20 times', async t => {
     await t
-        .maximizeWindow()
-
-        .typeText(page.input_field,data.task1)
-        .pressKey('enter')
 
         for (let i = 0; i < 20; i++) {
             await t
@@ -41,3 +37,36 @@ await t
     .expect(page.destroy_button_task1.exists).notOk();
 });
 
+test('Test case 3: edit a task using blanks', async t => {
+    await t
+        .typeText(page.input_field,data.task1)
+        .pressKey('enter')
+        .doubleClick(page.item_1)
+        .click(page.edit_item_1)
+        .selectText(page.edit_item_1)
+        .pressKey('delete')
+        .typeText(page.edit_item_1,"   ")
+        .pressKey('enter')
+
+        await t
+        .expect(page.todo_list.exists).notOk()
+        .expect(page.item_1.exists).notOk()
+        .expect(page.destroy_button_task1.exists).notOk();
+});
+
+test('Test case 4: create a task usin special characters and code', async t => {
+    await t
+        .typeText(page.input_field,data.task_special)
+        .pressKey('enter')
+        .typeText(page.input_field,data.task_code)
+        .pressKey('enter')
+
+
+
+    await t
+        .expect(page.todo_list.exists).ok()
+        .expect(page.item_1.exists).ok()
+        .expect(page.item_1.innerText).eql(data.task_special)
+        .expect(page.checkbox_task1.checked).eql(false)
+        .expect(page.destroy_button_task1.exists).ok();
+});
